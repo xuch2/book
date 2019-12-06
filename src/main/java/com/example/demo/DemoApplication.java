5e2631e90f0815576df59b539e4dffed2d25a2cb
xuch2 <wwts@nate.com>
2019-12-06 18:49:18 +0900

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+import com.example.demo.library.Book;
+import com.example.demo.library.BookService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@Bean
+	public ApplicationRunner booksInitializer(BookService bookService) {
+		return args -> {
+			bookService.create(new Book("9780061120084", "To Kill a Mockingbird", "Harper Lee"));
+			bookService.create(new Book("9780451524935", "1984", "George Orwell"));
+			bookService.create(new Book("9780618260300", "The Hobbit", "J.R.R. Tolkien"));
+		};
+	}
+}
